5101c4c5a18e13430275b752e7af55863ccd36a5
Guzel <guzel.r87@mail.ru>
2017-10-04 19:20:10 +0300

    Add project files.

---
+++ Wall/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+Вывести в ряд 1000 символов #.
+Циклы использовать нельзя.
+ */
+
+namespace Wall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new String('#', 1000));
+            Console.ReadKey();
+        }
+    }
+}
